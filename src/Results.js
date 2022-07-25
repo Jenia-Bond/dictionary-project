a70fabffff7297c22a24d703e71ce4f250992f19
Jenia-Bond <unassertive@gmail.com>
2022-07-25 13:09:05 +0200

    added word phonetics

--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Meaning from "./Meaning.js";
+import Phonetic from "./Phonetic";
 import "./Results.css";
 
 export default function Results(props) {
@@ -9,7 +10,15 @@ export default function Results(props) {
       <div className="Results">
         <div className="searched-word">
           <h2>{props.results.word}</h2>
-          <h3>{props.results.phonetic}</h3>
+          <h4>
+            {props.results.phonetics.map(function (phonetic, index) {
+              return (
+                <div key={index}>
+                  <Phonetic phonetic={phonetic} />
+                </div>
+              );
+            })}
+          </h4>
         </div>
         {props.results.meanings.map(function (meaning, index) {
           return (
